8c426f9995f6832662249dc45a26ca244db8f5e0
Khurram-Devs <ikhurram765@gmail.com>
2025-12-03 14:30:00 +0500

    Features Card Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import LogoSection from "./components/LogoSection"
+import LogoSection from "./sections/LogoSection"
 import NavBar from "./components/NavBar"
+import FeatureCard from "./sections/FeatureCard"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCard />
     </>
   )
 }
